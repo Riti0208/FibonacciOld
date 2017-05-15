a6374d4b336e26b1015b52284f9067a068b3e702
くらくら <daichizaki@yahoo.co.jp>
2017-05-15 21:45:21 +0900

    Commit

---
+++ fibonacciOld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace fibonacci{
+    class MainClass{
+        public static void Main(){
+            for (int i = 0; i < 100; i++){
+                Console.WriteLine("n : " + i.ToString() + " = " + fibonacci(i).ToString());
+            }
+        }
+        private static int fibonacci(int value){
+			if (value == 0) return 0;
+			if (value == 1) return 1;
+			return fibonacci(value - 2) + fibonacci(value - 1);
+        }
+
+    }
+}
